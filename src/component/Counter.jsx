264f093bdb02b2c98d1b10d74edf083bd93a8b20
Mohit Gupta <mohit.gupta08@nagarro.com>
2026-01-21 11:29:36 +0530

    file name fixed

---
+++ src/component/Counter.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  //create counter component in react js with increment and decrement button
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
